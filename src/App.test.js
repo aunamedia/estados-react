d340c527ac8116435ddfe3db49a5444d58db62af
Antonio García <agarcia@shsconsultores.es>
2021-10-17 15:50:45 +0200

    update texts

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/aprende react/i);
+  const linkElement = screen.getByText(/aprende ya react/i);
   expect(linkElement).toBeInTheDocument();
 });
